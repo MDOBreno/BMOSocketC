33740aa9fa399391db23b5de950b4ba13571dec5
Breno Medeiros de Oliveira <breno.medeiros@icloud.com>
2020-05-17 22:49:35 -0300

    Initial Commit

---
+++ BMOSocketC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  BMOSocketC
+//
+//  Created by Breno Medeiros on 17/05/20.
+//  Copyright © 2020 ProgramasBMO. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
